2878789b79b90e932e89ab7f2473bd216ee064a8
nursholihin62 <nur.sholihin.16@gmail.com>
2025-12-23 21:28:15 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,18 @@
-// 1. IMPORT (Wajib ada di paling atas)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 2. CONFIG (Isi dengan data dari layar Firebase Anda)
 const firebaseConfig = {
-    apiKey: "AIzaSyD...",                // <-- Pastikan kode ini lengkap sesuai dari Firebase
-    authDomain: "portal-sma-v1.firebaseapp.com",
-    projectId: "portal-sma-v1",
-    storageBucket: "portal-sma-v1.appspot.com",
-    messagingSenderId: "123456...",
-    appId: "1:123456..."
+  apiKey: "AIzaSyD8kwkwGOGswoCNBhNO5zD0pOHZKg5JIvk",
+  authDomain: "portal-belajar-smansarembang.firebaseapp.com",
+  projectId: "portal-belajar-smansarembang",
+  storageBucket: "portal-belajar-smansarembang.firebasestorage.app",
+  messagingSenderId: "573659279240",
+  appId: "1:573659279240:web:ad58bd2ad4d965fbb9543d"
 };
 
-// 3. INITIALIZE (Menghidupkan Firebase)
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// 4. EXPORT (Supaya bisa dipakai di file lain seperti auth.js)
 export { auth, db };
